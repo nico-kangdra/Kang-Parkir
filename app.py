3caf0d132724fb174f8e94fd8f7c98dd6ec5a958
Nico Kangdra <nkangdra@gmail.com>
2024-01-10 17:14:12 +0700

    add login feature

--- app.py
+++ app.py
@@ -1,47 +1,100 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, redirect, flash
 import os
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from datetime import timedelta
+from datetime import date
+from database import login, register, set_user, get_user, update_user
 
 app = Flask(__name__)
 app.secret_key = "LETSGOSPORT"
-limiter = Limiter(
-    get_remote_address, app=app, default_limits=["3/second"]
-)
+limiter = Limiter(get_remote_address, app=app, default_limits=["3/second"])
+
 
 @app.before_request
 def before_request():
     session.permanent = True
     app.permanent_session_lifetime = timedelta(weeks=2)
 
+
 @app.get("/")
-def main():
+def home_get():
     images = sorted(os.listdir(app.static_folder + "/carousel"))
     return render_template("index.html", images=images)
 
 
 @app.get("/courts")
-def courts():
+def courts_get():
     images = sorted(os.listdir(app.static_folder + "/courts"))
     return render_template("courts.html", images=images)
 
 
 @app.get("/booking")
-def booking():
+def booking_get():
     return render_template("booking.html")
 
+
 @app.get("/profile")
-def profile():
-    return render_template("profile.html")
+def profile_get():
+    data = get_user(session["email"])
+    return render_template("profile.html", data=data)
+
+
+@app.post("/profile")
+def profile_post():
+    name = request.form["name"]
+    age = request.form["age"]
+    interest = request.form["interest"]
+    data = {"name": name, "age": age, "interest": interest}
+    update_user(session["email"], data)
+    return redirect("/profile")
+
 
 @app.get("/login")
-def login():
+def login_get():
     return render_template("login.html")
 
+
+@app.post("/login")
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    log_in = login(email, password)
+    if log_in[0]:
+        session["token"] = log_in[1]
+        session["email"] = email
+        return redirect("/")
+    else:
+        session.clear()
+        return redirect("/login")
+
+
 @app.get("/register")
-def register():
+def register_get():
     return render_template("register.html")
+
+
+@app.post("/register")
+def register_post():
+    email = request.form["email"]
+    name = request.form["name"]
+    age = date.today().year - int(request.form["age"])
+    interest = request.form["interest"]
+    password = request.form["password"]
+
+    if get_user(email):
+        flash("Email already taken")
+        return redirect("/register")
+    else:
+        message = register(email, password)
+        set_user(email, password, name, age, interest)
+        flash(message)
+        return redirect("/login")
+
+@app.get("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import pyrebase
 from hashlib import sha256
 import json
+
 f = open("config.json")
 config = json.load(f)
 
@@ -10,30 +11,76 @@ db = firebase.database()
 storage = firebase.storage()
 
 
-def register(email, password):
+def encode(var: str):
+    return sha256(var.encode("utf-8")).hexdigest()
+
+
+def register(email: str, password: str):
     try:
         user = auth.create_user_with_email_and_password(email, password)
-        auth.send_email_verification(user['idToken'])
-        return "Verfication has been sent"
+        auth.send_email_verification(user["idToken"])
+        return "Verfication link has been sent to your Email"
     except:
-        return "There's an error"
+        return "There's an error occured"
 
-def login(email, password):
+
+def login(email: str, password: str):
     user = auth.sign_in_with_email_and_password(email, password)
-    return auth.get_account_info(user["idToken"])['users'][0]['emailVerified']
+    user = auth.refresh(user["refreshToken"])
+    return (auth.get_account_info(user["idToken"])["users"][0]["emailVerified"], user["idToken"])
 
 
 def set_user(email: str, password: str, name: str, age: int, interest: str):
     data = {
-        "email": email,
-        "password": sha256(password.encode()).hexdigest(),
-        "name": name,
-        "age": age,
-        "interest": interest,
+        encode(email): {
+            "email": email,
+            "password": encode(password),
+            "data": {
+                "name": name,
+                "age": age,
+                "interest": interest,
+            },
+        }
     }
-    db.child("users").set(data)
+    db.child("users").update(data)
 
-# def get_image():
+
+def get_user(email: str):
+    return db.child("users").child(encode(email)).get().val()
+
+
+def update_user(email: str, data: dict):
+    db.child("users").child(encode(email)).child("data").update(data)
+
+
+def set_court(court: str, location: str, link: str, type: str, image_filename: str):
+    data = {
+        encode(court): {
+            "name": court,
+            "location": location,
+            "link": link,
+            "type": type,
+            "image": image_filename,
+        }
+    }
+    db.child("courts").update(data)
+
+
+def get_court(court: str):
+    return db.child("courts").child(encode(court)).get().val()
+
+
+def update_court(court: str, data: dict):
+    db.child("courts").child(encode(court)).update(data)
+
+
+def set_timeslot(court: str, data:dict):
+    db.child("courts").child(encode(court)).child("timeslot").update(data)
+
+# set_court("Kharisma","Kalideres","null","Badminton","None")
+# set_timeslot("Kharisma")
+# print(get_court("Kharisma"))
+# def get_user(email:str)
 
 # user = register("nkangdra@gmail.com","aaaaaa")
 # print(user)

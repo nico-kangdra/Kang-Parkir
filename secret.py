f187d85e7028320c6eb2d7cbf4a2f85c2f81eaaf
nicokangdra <99784038+nicokangdra@users.noreply.github.com>
2024-05-01 10:55:20 +0700

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,20 @@
+X = [
+  {
+  "apiKey": "REPLACE WITH YOURS",
+  "authDomain": "REPLACE WITH YOURS",
+  "projectId": "REPLACE WITH YOURS",
+  "storageBucket": "REPLACE WITH YOURS",
+  "messagingSenderId": "REPLACE WITH YOURS",
+  "appId": "REPLACE WITH YOURS",
+  "measurementId": "REPLACE WITH YOURS",
+  "databaseURL": "REPLACE WITH YOURS"
+  },
+  {
+    "email": "REPLACE WITH YOURS",
+    "password": "REPLACE WITH YOURS",
+    "api_key": "REPLACE WITH YOURS"
+  },
+  {
+    "secret": "REPLACE WITH YOURS"
+  }
+]
